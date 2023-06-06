c7f9c63e46c90bf6aa06725a45cbe28bab58112f
fabri <fabri@host.docker.internal>
2023-06-06 08:52:46 -0400

    first commit

---
+++ LevioInterview/src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE jobposition
+(
+ id int NOT NULL,
+ nom varchar(150) DEFAULT NULL,
+ PRIMARY KEY (id)
+);
+
+
+test
---
+++ LevioInterview/src/test/java/ca/Levio/interview/InterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.Levio.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
